f1d4d4fd3de5252a41b25673792876e0ae86dbd5
sluzerpp <bistboy@list.ru>
2022-10-17 21:51:04 +0300

    feat: complete binary-search-tree task

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -6,41 +6,150 @@ const { NotImplementedError } = require('../extensions/index.js');
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+}
+
 class BinarySearchTree {
+  constructor() {
+    this.knot = null;
+  }
+  add(data) {
+    this.knot = addWithin(this.knot, data);
 
+    function addWithin(node, data) {
+      if (!node) {
+        return new Node(data);
+      }
+
+      if (node.data === data) {
+        return node;
+      }
+
+      if (data < node.data) {
+        node.left = addWithin(node.left, data);
+      } else {
+        node.right = addWithin(node.right, data);
+      }
+
+      return node;
+    }
+  }
+
+  has(data) {
+    return searchWithin(this.knot, data);
+
+    function searchWithin(node, data) {
+      if (!node) {
+        return false;
+      }
+
+      if (node.data === data) {
+        return true;
+      }
+
+      return data < node.data
+        ? searchWithin(node.left, data)
+        : searchWithin(node.right, data);
+    }
+  }
+
+  find(data) {
+    return searchWithin(this.knot, data);
+
+    function searchWithin(node, data) {
+      if (!node) {
+        return null;
+      }
+
+      if (node.data === data) {
+        return node;
+      }
+
+      return data < node.data
+        ? searchWithin(node.left, data)
+        : searchWithin(node.right, data);
+    }
+  }
+  remove(data) {
+    this.knot = removeNode(this.knot, data);
+
+    function removeNode(node, data) {
+      if (!node) {
+        return null;
+      }
+
+      if (data < node.data) {
+        node.left = removeNode(node.left, data);
+        return node;
+      } else if (node.data < data) {
+        node.right = removeNode(node.right, data);
+        return node;
+      } else {
+        if (!node.left && !node.right) {
+          return null;
+        }
+
+        if (!node.left) {
+          node = node.right;
+          return node;
+        }
+
+        if (!node.right) {
+          node = node.left;
+          return node;
+        }
+
+        let minFromRight = node.right;
+        while (minFromRight.left) {
+          minFromRight = minFromRight.left;
+        }
+        node.data = minFromRight.data;
+
+        node.right = removeNode(node.right, minFromRight.data);
+
+        return node;
+      }
+    }
+  }
+  // В этом методе мы должны найти "корень" нашего дерева
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let current = this.knot;
+    if (current === null) {
+      return null;
+    }
+    if (current.hasOwnProperty("data")) {
+      return current;
+    }
   }
-
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-
   min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (!this.knot) {
+      return;
+    }
+
+    let node = this.knot;
+    while (node.left) {
+      node = node.left;
+    }
+
+    return node.data;
   }
 
   max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (!this.knot) {
+      return;
+    }
+
+    let node = this.knot;
+    while (node.right) {
+      node = node.right;
+    }
+
+    return node.data;
   }
 }
 
